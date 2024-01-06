052a09c224691b76cb1e10c630ee209c63a869d5
Priscillalala <95263024+Priscillalala@users.noreply.github.com>
2024-01-05 18:17:14 -0800

    upgrade single material

--- ShaderSwapper/ShaderSwapper.cs
+++ ShaderSwapper/ShaderSwapper.cs
@@ -7,7 +7,6 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
-using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace ShaderSwapper
@@ -36,59 +35,32 @@ namespace ShaderSwapper
             {
                 yield break;
             }
+
             List<AsyncOperationHandle> loadResourceLocationsOperations = new List<AsyncOperationHandle>(materialCount);
-            /*foreach (Material material in loadMaterials.allAssets)
-            {
-                string cachedShaderName = material.shader.name;
-                if (cachedShaderName.StartsWith(PREFIX))
-                {
-                    materials.Add(material);
-                    string key = cachedShaderName.Substring(PREFIX_LENGTH) + ".shader";
-                    loadResourceLocationsOperations.Add(Addressables.LoadResourceLocationsAsync(key, typeof(Shader)));
-                }
-            }*/
             for (int i = materialCount - 1; i >= 0; i--)
             {
                 string cachedShaderName = ((Material)allMaterials[i]).shader.name;
                 if (cachedShaderName.StartsWith(PREFIX))
                 {
-                    string key = cachedShaderName.Substring(PREFIX_LENGTH) + ".shader";
-                    loadResourceLocationsOperations.Add(Addressables.LoadResourceLocationsAsync(key, typeof(Shader)));
+                    loadResourceLocationsOperations.Add(Addressables.LoadResourceLocationsAsync(cachedShaderName.Substring(PREFIX_LENGTH) + ".shader", typeof(Shader)));
                 }
                 else
                 {
-                    ArrayRemoveAt(allMaterials, i, ref materialCount);
+                    ArrayRemoveAtNoCleanup(allMaterials, i, ref materialCount);
                 }
             }
+            Debug.Log($"loadResourceLocationsOperations count: {loadResourceLocationsOperations.Count}");
             if (materialCount <= 0)
             {
                 yield break;
             }
-            Debug.Log($"loadResourceLocationsOperations count: {loadResourceLocationsOperations.Count}");
 
             AsyncOperationHandle<IList<AsyncOperationHandle>> loadResourceLocationsGroup = Addressables.ResourceManager.CreateGenericGroupOperation(loadResourceLocationsOperations);
             if (!loadResourceLocationsGroup.IsDone)
             {
                 yield return loadResourceLocationsGroup;
             }
-            Debug.Log($"allResourceLocations count: {loadResourceLocationsGroup.Result.Count}");
             List<IResourceLocation> resourceLocations = new List<IResourceLocation>(materialCount);
-            /*int j = 0;
-            foreach (AsyncOperationHandle operation in loadResourceLocationsGroup.Result)
-            {
-                IList<IResourceLocation> result = (IList<IResourceLocation>)operation.Result;
-                if (result.Count > 0)
-                {
-                    Debug.Log($"added location: {result[0].PrimaryKey}");
-                    resourceLocations.Add(result[0]);
-                    j++;
-                }
-                else
-                {
-                    materials.RemoveAt(j);
-                }
-            }*/
-
             for (int i = materialCount - 1; i >= 0; i--)
             {
                 IList<IResourceLocation> result = (IList<IResourceLocation>)loadResourceLocationsGroup.Result[i].Result;
@@ -99,20 +71,19 @@ namespace ShaderSwapper
                 }
                 else
                 {
-                    ArrayRemoveAt(allMaterials, materialCount - 1 - i, ref materialCount);
+                    ArrayRemoveAtNoCleanup(allMaterials, materialCount - 1 - i, ref materialCount);
                 }
             }
             if (materialCount <= 0)
             {
                 yield break;
             }
+
             AsyncOperationHandle<IList<Shader>> loadShaders = Addressables.LoadAssetsAsync<Shader>(resourceLocations, null, false);
             if (!loadShaders.IsDone)
             {
                 yield return loadShaders;
             }
-            Debug.Log(loadShaders.Result != null);
-            Debug.Log(loadShaders.Result.Count);
             int startIndex = _.Length;
             Array.Resize(ref _, startIndex + materialCount);
             for (int i = 0; i < materialCount; i++)
@@ -121,29 +92,48 @@ namespace ShaderSwapper
                 _[startIndex + i] = allMaterials[i];
                 Debug.Log($"set real shader on mat {((Material)allMaterials[i]).name} to {loadShaders.Result[i]?.name ?? "null"}");
             }
-            Debug.Log($"_ length: {_.Length}");
+        }
+
+        public static IEnumerator UpgradeStubbedShaderAsync(Material material)
+        {
+            if (material == null)
+            {
+                throw new ArgumentNullException(nameof(material));
+            }
+            string cachedShaderName = material.shader.name;
+            if (!cachedShaderName.StartsWith(PREFIX))
+            {
+                yield break;
+            }
+            AsyncOperationHandle<IList<IResourceLocation>> loadResourceLocations = Addressables.LoadResourceLocationsAsync(cachedShaderName.Substring(PREFIX_LENGTH) + ".shader", typeof(Shader));
+            if (!loadResourceLocations.IsDone)
+            {
+                yield return loadResourceLocations;
+            }
+            if (loadResourceLocations.Result.Count <= 0)
+            {
+                yield break;
+            }
+            AsyncOperationHandle<Shader> loadShader = Addressables.LoadAssetAsync<Shader>(loadResourceLocations.Result[0]);
+            if (!loadShader.IsDone)
+            {
+                yield return loadShader;
+            }
+            material.shader = loadShader.Result;
+            int index = _.Length;
+            Array.Resize(ref _, index + 1);
+            _[index] = material;
+            Debug.Log($"set real shader on mat {material} to {loadShader.Result?.name ?? "null"}");
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void ArrayRemoveAt<T>(T[] array, int index, ref int length)
+        private static void ArrayRemoveAtNoCleanup<T>(T[] array, int index, ref int length)
         {
             length--;
             for (int i = index; i < length; i++)
             {
                 array[i] = array[i + 1];
             }
-        }
-    }
-
-    [BepInPlugin("com.groovesalad.TestPlugin", "TestPlugin", "1.0.0")]
-    public class TestPlugin : BaseUnityPlugin
-    {
-        public AssetBundle testBundle;
-        public void OnEnable()
-        {
-            Logger.LogInfo("Awake!");
-            testBundle ??= AssetBundle.LoadFromFile(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), "testassets"));
-            StartCoroutine(testBundle.UpgradeStubbedShadersAsync());
         }
     }
 }
---
+++ ShaderSwapper/TestPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace ShaderSwapper
+{
+    [BepInPlugin("com.groovesalad.TestPlugin", "TestPlugin", "1.0.0")]
+    public class TestPlugin : BaseUnityPlugin
+    {
+        public AssetBundle testBundle;
+        public void OnEnable()
+        {
+            Logger.LogInfo("Awake!");
+            testBundle ??= AssetBundle.LoadFromFile(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Info.Location), "testassets"));
+            StartCoroutine(testBundle.UpgradeStubbedShadersAsync());
+            Material test1 = testBundle.LoadAsset<Material>("matTest4");
+            StartCoroutine(ShaderSwapper.UpgradeStubbedShaderAsync(test1));
+        }
+    }
+}
